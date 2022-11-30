7d09b5737c6a43253da5defb1c55c207b0c15862
Daniil <Comm2k3@hotmail.com>
2022-11-30 13:07:52 +0200

    Ready

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int result = 0;
+Console.Write("Enter 1st number ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter 2nd number ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+result = number1;
+else
+result = number2;
+Console.WriteLine("Число " + result + " наибольшее из двух чисел");
